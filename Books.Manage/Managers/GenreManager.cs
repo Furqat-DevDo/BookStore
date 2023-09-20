e6e11959f15f1ac9c91d4c4c278c27c4b6173175
Shohjahon727 <135617343+Shohjahon727@users.noreply.github.com>
2023-09-20 19:31:05 +0500

    Genre

--- Books.Manage/Managers/GenreManager.cs
+++ Books.Manage/Managers/GenreManager.cs
@@ -12,13 +12,13 @@ public class GenreManager : IGenreManager
 {
     private readonly ILogger<GenreModel> _logger;
     private readonly BookDbContext _dbContext;
-    private readonly IGenreGuardian _guardian;
+    private readonly IGuardian _guardian;
     private readonly IGenreMapper _mapper;
     private readonly IGenreRepository _repository;
 
     public GenreManager(ILogger<GenreModel> logger,
         BookDbContext dbContext,
-        IGenreGuardian guardian,
+        IGuardian guardian,
         IGenreMapper mapper,
         IGenreRepository repository)
     {
@@ -30,7 +30,7 @@ public class GenreManager : IGenreManager
     }
     public async Task<GenreModel> CreateGenreAsync(CreateGenreModel model)
     {
-        await _guardian.GernreGuardNull(model);
+        await _guardian.GuardAgainstNull(model);
         _mapper.ToEntity(model);
         var entity = await _repository.CreateGenre(_mapper.ToEntity(model));
         return _mapper.ToModel(entity);
@@ -38,8 +38,8 @@ public class GenreManager : IGenreManager
 
     public Task<bool> DeleteGenreAsync(int id)
     {
-        _guardian.GernreGuardZero(id);
-        _guardian.GernreGuardMinus(id);
+        _guardian.GuardAgainstZero(id);
+        _guardian.GuardAgainstMinus(id);
         return _repository.DeleteById(id);
     }
 
@@ -52,8 +52,8 @@ public class GenreManager : IGenreManager
 
     public async Task<GenreModel> GetGenreByIdAsync(int id)
     {
-        await _guardian.GernreGuardZero(id);
-        await _guardian.GernreGuardMinus(id);
+        await _guardian.GuardAgainstZero(id);
+        await _guardian.GuardAgainstMinus(id);
         var genre = await _repository.GetById(id);
         if (genre is null) throw new GenreNotFoundException(nameof(genre));
         return _mapper.ToModel(genre);
@@ -61,7 +61,7 @@ public class GenreManager : IGenreManager
 
     public async Task<GenreModel> GetGenreNameAsync(string name)
     {
-        await _guardian.GernreGuardNullOrEmptyString(name);
+        await _guardian.GuardAgainstNullOrEmptyString(name);
         var genre = await _repository.GetGenreByFilter(b => b.Name == name);
         if (genre is null) throw new GenreNotFoundException(nameof(genre));
         return _mapper.ToModel(genre);
@@ -69,7 +69,7 @@ public class GenreManager : IGenreManager
 
     public async Task<GenreModel> UpdateGenreAsync(int id, CreateGenreModel model)
     {
-        await _guardian.GernreGuardNull(model);
+        await _guardian.GuardAgainstNull(model);
         _mapper.ToEntity(model);
         var genre = await _repository.GetById(id);
         if (genre is null) throw new GenreNotFoundException(nameof(genre));
