c3fc1a7b386fda4029d30e8de9976f953056b7ee
Shohjahon727 <135617343+Shohjahon727@users.noreply.github.com>
2023-09-20 19:11:17 +0500

    Genre

---
+++ Books.Manage/Helpers/Validators/GenreGuardian.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Books.Manage.Helpers.Validators;
+
+public class GenreGuardian : IGenreGuardian
+{
+    public Task GernreGuardMinus<T>(T input) where T : IComparable, IEquatable<T>
+    {
+       if(Convert.ToDouble(input) < 0) throw new ArgumentOutOfRangeException(nameof(input));
+       return Task.CompletedTask;
+    }
+
+    public Task GernreGuardNull<T>(T input) 
+    {
+        if (input == null) throw new ArgumentNullException(nameof(input));
+        return Task.CompletedTask;
+    }
+
+    public Task GernreGuardNullOrEmptyString<T>(T input) where T : IComparable
+    {
+        if(string.IsNullOrWhiteSpace(Convert.ToString(input,CultureInfo.InvariantCulture))) 
+            throw new ArgumentNullException(nameof(input));
+        return Task.CompletedTask;
+    }
+
+    public Task GernreGuardZero<T>(T input) where T : IComparable,IEquatable<T>
+    {
+       if(Convert.ToDouble(input) == 0) throw new NotImplementedException(nameof(input));
+       return Task.CompletedTask;
+    }
+}
---
+++ Books.Manage/Helpers/Validators/IGenreGuardian.cs
@@ -0,0 +1,9 @@
+﻿namespace Books.Manage.Helpers.Validators;
+
+public interface IGenreGuardian
+{
+    Task GernreGuardNull<T>(T input);
+    Task GernreGuardZero<T>(T input) where T : IComparable, IEquatable<T>;
+    Task GernreGuardMinus<T>(T input) where T : IComparable, IEquatable<T>;
+    Task GernreGuardNullOrEmptyString<T>(T input) where T : IComparable;
+}
