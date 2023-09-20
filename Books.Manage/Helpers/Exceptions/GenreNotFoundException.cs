882e514881ef217fbfd9d85220e5a60171f91253
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2023-09-20 19:21:48 +0500

    Revert "Genre"

--- Books.Manage/Helpers/Exceptions/GenreNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-namespace Books.Manage.Helpers.Exceptions;
-
-public class GenreNotFoundException : Exception
-{
-    public GenreNotFoundException(string message) : base(message) { }
-}
--- Books.Manage/Managers/Abstractions/IGenreManager.cs
+++ Books.Manage/Managers/Abstractions/IGenreManager.cs
@@ -1,15 +1,8 @@
 ﻿namespace Books.Manage.Managers.Abstractions;
 
 public record GenreModel(int Id, string Name);
-public record CreateGenreModel(int Id, string Name );
 
 public interface IGenreManager
 {
-    Task<GenreModel> CreateGenreAsync(CreateGenreModel model);
-    Task<GenreModel> UpdateGenreAsync(int id,CreateGenreModel model);
-    Task<bool> DeleteGenreAsync(int id);
-    Task<GenreModel> GetGenreByIdAsync(int id);
-    Task<GenreModel> GetGenreNameAsync(string name);
-    Task<List<GenreModel>> GetAllGenreAsync();
-}
 
+}
