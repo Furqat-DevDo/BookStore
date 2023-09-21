721fd9f58c25b85a13bc20a88d402f765b0eec63
BehruzbekUmarov <bekhruz476@gmail.com>
2023-09-21 04:49:55 -0800

    daasd

--- Books.Manage/Repositories/WriterRepository.cs
+++ Books.Manage/Repositories/WriterRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Books.Core.Data;
+using Books.Core.Entities;
+using Books.Manage.Repositories.Abstarctions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,10 @@ using System.Threading.Tasks;
 
 namespace Books.Manage.Repositories
 {
-    internal class WriterRepository
+    public class WriterRepository : GenericRepository<Writer, BookDbContext>, IWriterRepository
     {
+        public WriterRepository(BookDbContext tContext) : base(tContext)
+        {
+        }
     }
 }
