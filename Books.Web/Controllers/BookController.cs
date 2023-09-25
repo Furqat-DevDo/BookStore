910d5db2731bee71d56dd0ee830a68001c129dd8
Furqat-DevDo <furqata93@gmail.com>
2023-09-25 19:27:39 +0500

    Book Controller

--- Books.Web/Controllers/BookController.cs
+++ Books.Web/Controllers/BookController.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Books.Manage.Managers.Abstractions;
+using Books.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Books.Web.Controllers;
 
 public class BookController : Controller
 {
+    private readonly IBookManager _bookManager;
+    private readonly IGenreManager _genreManager;
+    private readonly IWriterManager _writerManager;
+    private readonly IBookSeriesManager _bookSeriesManager;
+
+    public BookController(
+        IBookManager bookManager, 
+        IGenreManager genreManager, 
+        IWriterManager writerManager, 
+        IBookSeriesManager bookSeriesManager)
+    {
+        _bookManager = bookManager;
+        _genreManager = genreManager;
+        _writerManager = writerManager;
+        _bookSeriesManager = bookSeriesManager;
+    }
+
     public IActionResult Home()
     {
         return View();
@@ -19,13 +38,18 @@ public class BookController : Controller
         return View();
     }
 
-    public IActionResult Search()
+    [HttpPost]
+    public async Task<IActionResult> Search(SearchModel model)
     {
+        var result = new List<BookModel>();
+
+        var book = await _bookManager.GetBookByNameAsync(model.Filter);
+        result.Add(book);
+
+        
+
+        //TODO RedirectToAction(ListBook)
         return View();
     }
 
-    public IActionResult Contact()
-    {
-        return View();
-    }
 }
---
+++ Books.Web/Models/SearchModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Books.Web.Models;
+
+public class SearchModel
+{
+    [NotNull]
+    public required string Filter { get; set; }
+}
