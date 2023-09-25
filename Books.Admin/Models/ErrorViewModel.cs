9f2880d8676a690c3bf32d9a39d003fc6d6a5ce2
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-09-25 19:28:23 +0500

    project added

---
+++ Books.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Books.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
