785395b337ca57c23ce5bfab3e1d4cdf2a4dd783
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-09-25 17:38:21 +0500

    Need materials added

---
+++ Books.Web/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Web.Controllers;
+
+public class AdminController : Controller
+{
+    public IActionResult Dashboard()
+    {
+        return View("dashboard");
+    }
+}
