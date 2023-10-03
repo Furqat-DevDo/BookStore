fe703a3649ec87b2a2534b979db5e0298ffb58ca
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-10-03 19:06:03 +0500

    view done

--- Books.Web/Models/BookCreateViewModel.cs
+++ Books.Web/Models/BookCreateViewModel.cs
@@ -1,12 +1,17 @@
-﻿namespace Books.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Web.Models;
 
 public class BookCreateViewModel
 {
-    public string Name { get; set; }
+    [MinLength(5, ErrorMessage = "Length of book name must be more than 4 characters")]
+    public required string Name { get; set; }
+
+
     public string Description { get; set; }
-    public string WriterName { get; set; }
-    public List<string> Genres { get; set; }
-    public IFormFile BookFile { get; set; }
+    public required string WriterName { get; set; }
+    public required List<string> Genres { get; set; }
+    public required IFormFile BookFile { get; set; }
     public IFormFile CoverImage { get; set; }
-    public decimal Price { get; set; }
+    public  decimal Price { get; set; }
 }
